b5a8b954b00c8b8afd8037c998f97a8575aabc6e
Furkan Ozdemir <furkan@ibis.ai>
2026-08-13 20:52:05 +0300

    Release v0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.frknzd/re-frame.vertica "0.1.2"
+(defproject net.clojars.frknzd/re-frame.vertica "0.2.0"
   :description "A Chrome DevTools vertical slice through re-frame data flow"
   :url "https://github.com/frknzd/re-frame.vertica"
   :license {:name "MIT"
