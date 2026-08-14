a5d1e4d7be9ea381de696b356d80d5649b0c45fc
Furkan Ozdemir <furkan@ibis.ai>
2026-08-15 01:38:00 +0300

    Release v0.3.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.frknzd/re-frame.vertica "0.3.4"
+(defproject net.clojars.frknzd/re-frame.vertica "0.3.5"
   :description "An in-app vertical slice through re-frame data flow"
   :url "https://github.com/frknzd/re-frame.vertica"
   :license {:name "MIT"
